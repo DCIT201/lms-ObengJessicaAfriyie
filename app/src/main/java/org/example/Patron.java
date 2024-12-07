194c0f17e4abc80433dff91806c94eb6de31f4d9
ObengJessicaAfriyie <jaobeng018@st.ug.edu.gh>
2024-12-07 17:34:04 +0000

    Create Patron.java

---
+++ app/src/main/java/org/example/Patron.java
@@ -0,0 +1,38 @@
+package ;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patron {
+
+        private String name;
+        private List<Book> borrowedBooks;
+
+        // Constructor
+        public Patron(String name) {
+            this.name = name;
+            this.borrowedBooks = new ArrayList<>();
+        }
+
+        // Getter for name
+        public String getName() {
+            return name;
+        }
+
+        // Method to borrow a book
+        public void borrowBook(Book book) {
+            borrowedBooks.add(book);
+        }
+
+        // Method to return a book
+        public void returnBook(Book book) {
+            borrowedBooks.remove(book);
+        }
+
+        // Method to list borrowed books
+        public void listBorrowedBooks() {
+            System.out.println(name + "'s Borrowed Books:");
+            for (Book book : borrowedBooks) {
+                book.displayDetails();
+            }
+        }
+    }
